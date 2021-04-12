b4aa7d56c06e0211bc1a6ea3a42e480dfc4c108e
Arne Dumarey <arne.dumarey@vlaanderen.be>
2021-04-12 14:54:49 +0200

    fix: revert "fix: testing aggregate 'Then' with identifier filters out the specific aggregate"
    This reverts commit 01d79f7a0a6319fc3dac7bcd6652e33541e87771.

--- src/Be.Vlaanderen.Basisregisters.AggregateSource.Testing.SqlStreamStore/StreamStoreFactRepository.cs
+++ src/Be.Vlaanderen.Basisregisters.AggregateSource.Testing.SqlStreamStore/StreamStoreFactRepository.cs
@@ -3,6 +3,7 @@ namespace Be.Vlaanderen.Basisregisters.AggregateSource.Testing.SqlStreamStore
     using System;
     using System.Collections.Generic;
     using System.Linq;
+    using System.Reflection;
     using System.Threading.Tasks;
     using EventHandling;
     using global::SqlStreamStore;
@@ -30,21 +31,7 @@ namespace Be.Vlaanderen.Basisregisters.AggregateSource.Testing.SqlStreamStore
         public async Task<Fact[]> RetrieveFacts(long fromPositionExclusive)
         {
             var results = new List<Fact>();
-            var page = await _streamStore.ReadAllForwards(fromPositionExclusive < 0 ? Position.Start : fromPositionExclusive, 10);
-            results.AddRange(page.Messages.Where(m => m.Position != fromPositionExclusive).Select(MapToFact));
-            while (!page.IsEnd)
-            {
-                page = await page.ReadNext();
-                results.AddRange(page.Messages.Where(m => m.Position != fromPositionExclusive).Select(MapToFact));
-            }
-
-            return results.ToArray();
-        }
-
-        public async Task<Fact[]> RetrieveFactsByStream(long fromPositionExclusive, string aggregateIdentifier)
-        {
-            var results = new List<Fact>();
-            var page = await _streamStore.ReadStreamForwards(new StreamId(aggregateIdentifier), fromPositionExclusive < 0 ? Convert.ToInt32(Position.Start) : Convert.ToInt32(fromPositionExclusive), 10);
+            var page = await _streamStore.ReadAllForwards(fromPositionExclusive<0?Position.Start:fromPositionExclusive, 10);
             results.AddRange(page.Messages.Where(m => m.Position != fromPositionExclusive).Select(MapToFact));
             while (!page.IsEnd)
             {
@@ -70,7 +57,7 @@ namespace Be.Vlaanderen.Basisregisters.AggregateSource.Testing.SqlStreamStore
                             _eventMapping.GetEventName(e.Event.GetType()),
                             _eventSerializer.SerializeObject(e.Event))).ToArray());
 
-            return result?.CurrentPosition ?? await _streamStore.ReadHeadPosition();
+            return result?.CurrentPosition??await _streamStore.ReadHeadPosition();
         }
 
         private Fact MapToFact(StreamMessage streamMessage)

b3ed9b470e0ba50714c543e86bc6f9c828e4464a
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-05-02 15:54:47 +0200

    feat: add AfterEventsCountStrategy, AfterEventTypeStrategy

---
+++ src/Be.Vlaanderen.Basisregisters.AggregateSource/Snapshotting/AfterEventTypeStrategy.cs
@@ -0,0 +1,17 @@
+namespace Be.Vlaanderen.Basisregisters.AggregateSource.Snapshotting
+{
+    using System;
+    using System.Linq;
+
+    public class AfterEventTypeStrategy : ISnapshotStrategy
+    {
+        private readonly Type[] _eventTypes;
+
+        public AfterEventTypeStrategy(params Type[] eventTypes)
+        {
+            _eventTypes = eventTypes;
+        }
+
+        public bool ShouldCreateSnapshot(SnapshotStrategyContext context) => _eventTypes.Contains(context.Events.Last().GetType());
+    }
+}

52d2b6f6b494407a5602576e3c5ad29da5f2a9a1
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-05-11 12:59:13 +0200

    refactor: add NoSnapshotStrategyTests

---
+++ test/Be.Vlaanderen.Basisregisters.AggregateSource.Tests/Snapshotting/NoSnapshotStrategyTests.cs
@@ -0,0 +1,19 @@
+namespace Be.Vlaanderen.Basisregisters.AggregateSource.Tests.Snapshotting
+{
+    using System.Collections.Immutable;
+    using AggregateSource.Snapshotting;
+    using Xunit;
+
+    public class NoSnapshotStrategyTests
+    {
+        [Fact]
+        public void NeverTakeSnapshot()
+        {
+            var strategy = (ISnapshotStrategy)new NoSnapshotStrategy();
+            var ctx = new SnapshotStrategyContext(new Aggregate("1", 1, new AggregateRootEntityStub()), ImmutableList<EventWithMetadata>.Empty, 1, 0);
+            var result = strategy.ShouldCreateSnapshot(ctx);
+
+            Assert.False(result);
+        }
+    }
+}

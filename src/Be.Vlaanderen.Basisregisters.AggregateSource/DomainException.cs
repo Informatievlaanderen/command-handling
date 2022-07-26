0fa3131651804fe7d2bccb126c91ed47c9d72ffa
Luk Vermeulen <luk.vermeulen@gmail.com>
2022-07-26 19:17:33 +0200

    fix: make DomainException Serialiable-conformant

--- src/Be.Vlaanderen.Basisregisters.AggregateSource/DomainException.cs
+++ src/Be.Vlaanderen.Basisregisters.AggregateSource/DomainException.cs
@@ -1,13 +1,24 @@
 namespace Be.Vlaanderen.Basisregisters.AggregateSource
 {
     using System;
+    using System.Runtime.Serialization;
 
+    [Serializable]
     public abstract class DomainException : Exception
     {
-        protected DomainException() { }
+        protected DomainException()
+        { }
 
-        protected DomainException(string message) : base(message) { }
+        protected DomainException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        { }
 
-        protected DomainException(string message, Exception inner) : base(message, inner) { }
+        protected DomainException(string message)
+            : base(message)
+        { }
+
+        protected DomainException(string message, Exception inner)
+            : base(message, inner)
+        { }
     }
 }

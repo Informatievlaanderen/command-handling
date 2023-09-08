312295fcf26a19ba218c5911a0174c44a724c42d
emalfroy <egon.malfroy@gmail.com>
2023-09-08 11:40:10 +0200

    fix: snapshot verifier registrations

--- src/Be.Vlaanderen.Basisregisters.SnapshotVerifier/SnapshotVerifierExtensions.cs
+++ src/Be.Vlaanderen.Basisregisters.SnapshotVerifier/SnapshotVerifierExtensions.cs
@@ -3,29 +3,28 @@
     using System;
     using System.Collections.Generic;
     using AggregateSource;
+    using EventHandling;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
     using Microsoft.Extensions.Logging;
+    using SqlStreamStore;
 
     public static class SnapshotVerifierExtensions
     {
-        public static IServiceCollection AddSnapshotVerificationServices<TAggregateRoot, TStreamId>(
+        public static IServiceCollection AddSnapshotVerificationServices(
             this IServiceCollection services,
             string snapshotConnectionString,
             string snapshotSchema,
             string snapshotVerificationStatesTableName = "SnapshotVerificationStates")
-            where TAggregateRoot : class, IAggregateRootEntity, ISnapshotable
-            where TStreamId : class
         {
             services.AddSingleton(new MsSqlSnapshotStoreQueries(snapshotConnectionString, snapshotSchema));
             services.AddScoped<ISnapshotVerificationRepository>(_ => new SnapshotVerificationRepository(snapshotConnectionString, snapshotSchema, snapshotVerificationStatesTableName));
-            services.AddScoped<IAggregateSnapshotRepository<TAggregateRoot>, AggregateSnapshotRepository<TAggregateRoot>>();
-            services.AddScoped<IAggregateEventsRepository<TAggregateRoot, TStreamId>, AggregateEventsRepository<TAggregateRoot, TStreamId>>();
             return services;
         }
 
         public static IServiceCollection AddHostedSnapshotVerifierService<TAggregateRoot, TStreamId>(
             this IServiceCollection services,
+            Func<TAggregateRoot> aggregateFactory,
             Func<TAggregateRoot, TStreamId> aggregateIdFactory,
             List<string> membersToIgnoreInVerification)
             where TAggregateRoot : class, IAggregateRootEntity, ISnapshotable
@@ -36,8 +35,17 @@
                 aggregateIdFactory,
                 membersToIgnoreInVerification,
                 x.GetRequiredService<ISnapshotVerificationRepository>(),
-                x.GetRequiredService<IAggregateSnapshotRepository<TAggregateRoot>>(),
-                x.GetRequiredService<IAggregateEventsRepository<TAggregateRoot, TStreamId>>(),
+                new AggregateSnapshotRepository<TAggregateRoot>(
+                    x.GetRequiredService<MsSqlSnapshotStoreQueries>(),
+                    x.GetRequiredService<EventDeserializer>(),
+                    x.GetRequiredService<EventMapping>(),
+                    aggregateFactory,
+                    x.GetRequiredService<ILoggerFactory>()),
+                new AggregateEventsRepository<TAggregateRoot, TStreamId>(
+                    x.GetRequiredService<EventDeserializer>(),
+                    x.GetRequiredService<EventMapping>(),
+                    x.GetRequiredService<IReadonlyStreamStore>(),
+                    aggregateFactory),
                 x.GetService<ISnapshotVerificationNotifier>(), x.GetRequiredService<ILoggerFactory>()));
 
             return services;
